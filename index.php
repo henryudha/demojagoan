449390b0a9819e521140cca87f53b0ed3ee6c690
Henry Yudha <henryudha@gmail.com>
2022-09-19 13:27:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 </style>
 
 <article>
-    <h1>Wait a moments yaaaaa!</h1>
+    <h1>Please Wait!</h1>
     <div>
         <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:#">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
         <p>&mdash; The Team</p>
