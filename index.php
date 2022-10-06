334b63bb8d2567e17f6dd727700aebc4166f814e
Henry Yudha <henryudha@gmail.com>
2022-10-06 12:21:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <article>
     <h1>Please Wait!</h1>
     <div>
-        <p>Halloooo Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:yudha@jagoanhosting.com">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
+        <p>Halloooo Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:yudha@jagoan.cloud">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
         <p>&mdash; The Team</p>
     </div>
 </article>
