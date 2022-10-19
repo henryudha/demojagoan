9d35ba7b2062b7896c13eb5c86ba413dbd825f73
Henry Yudha <henryudha@gmail.com>
2022-10-19 15:28:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 </style>
 
 <article>
-    <h1>Please Wait!</h1>
+    <h1>Please Wait Gann!</h1>
     <div>
         <p>Halloooo Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:yudha@jagoan.cloud">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
         <p>&mdash; The Team</p>
